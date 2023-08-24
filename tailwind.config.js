165dba40957532bc2f737fe23fcad6ceb1e36afb
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2023-08-24 16:50:21 +0700

    Fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/templates/*.html', './src/**/*.{html,js}'],
+  content: ['./src/templates/*.html', './src/**/*.{html,js,ts}'],
   theme: {
     extend: {},
   },
